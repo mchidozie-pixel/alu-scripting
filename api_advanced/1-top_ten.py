0f393c9a8776c5bbfb898fc31303b8da3a7a57f7
mchidozie-pixel <m.chidozie@alustudent.com>
2026-02-23 19:42:17 +0000

    1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""Fetch top 10 hot posts from Reddit"""
+"""Print top 10 hot post titles for a subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print titles of first 10 hot posts"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    """Main function"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": 10}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    try:
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
 
-    if response.status_code != 200:
+        if response.status_code != 200:
+            print(None)
+            return
+
+        posts = response.json().get("data", {}).get("children", [])
+
+        if not posts:
+            print(None)
+            return
+
+        for post in posts:
+            print(post.get("data").get("title"))
+
+    except Exception:
         print(None)
-        return
-
-    data = response.json().get("data", {}).get("children", [])
-
-    if not data:
-        print(None)
-        return
-
-    for post in data:
-        print(post.get("data").get("title"))
-
